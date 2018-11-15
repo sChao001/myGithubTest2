c98f1ad83672d310538867183c0ab20bd5053db8
shaochao <15201152903@163.com>
2018-11-15 17:59:39 +0800

    测试

---
+++ sds/sds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sds
+//
+//  Created by Surdot on 2018/11/15.
+//  Copyright © 2018年 Surdot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sds/sds/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sds
+//
+//  Created by Surdot on 2018/11/15.
+//  Copyright © 2018年 Surdot. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
